3cd385fa4b62a180b29955b7c352ff2fff19d624
Fedir Komarovskyi <150347261+Fedir05@users.noreply.github.com>
2025-08-28 19:38:58 +0000

    Add new partial

--- js/index.js
+++ js/index.js
@@ -1,1 +1,13 @@
-import('./index/coffee-machine-carousel'); 
+
+function init() {
+    import('./index/coffee-machine-carousel.js');
+    import('./index/faq.partial.js');
+}
+
+const totalPartials = document.querySelectorAll('[hx-trigger="load"], [data-hx-trigger="load"]').length;
+let loadedPartialsCount = 0;
+
+document.body.addEventListener('htmx:afterOnLoad', () => {
+    loadedPartialsCount++;
+    if (loadedPartialsCount === totalPartials) init();
+});
